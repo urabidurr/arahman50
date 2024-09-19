afd2e1e17702285caf3cf2980177054a913b4646
urabidurr <arahman50@stuy.edu>
2024-09-19 10:58:07 -0400

    worked on K06

--- 06_py-csv/numbercruncher.py
+++ 06_py-csv/numbercruncher.py
@@ -1,1 +1,31 @@
+#Abidur Rahman
+#AMA
+#SoftDev
+#K06 -- Read from file, return job class based on specified percentage in data set
+#Time spent: 0.2
 
+
+fCVS = open("occupations.csv")
+
+stf = fCVS.read().strip()
+
+
+def createDict(text):
+    #print(text)
+    l = text.split("\n")
+    #print(l)
+    
+    dicts = {}
+    pair = []
+    
+    for i in l:
+        if i[0] == '"':
+            pair = i.split('",')
+            pair[0] = pair[0][1:]
+            print(pair)
+        
+        
+        
+    #print(dicts)
+    
+createDict(stf)
