53baa8ab3866946d85b89650888010bfc3d1582a
urabidurr <arahman50@stuy.edu>
2024-09-20 11:01:41 -0400

    updated

--- 06_py-csv/numbercruncher.py
+++ 06_py-csv/numbercruncher.py
@@ -2,33 +2,37 @@
 #AMA
 #SoftDev
 #K06 -- Read from file, return job class based on specified percentage in data set
-#Time spent: 0.2
+#Time spent: 0.8
 
-
+import random
 fCVS = open("occupations.csv")
 
 stf = fCVS.read().strip()
+dicts = {}
 
 
 def createDict(text):
     #print(text)
     l = text.split("\n")
     #print(l)
-    
-    dicts = {}
     pair = []
     
-    for i in l:
-        if i[0] == '"':
-            pair = i.split('",')
+    for i in range(1,len(l) - 1):
+        if l[i][0] == '"':
+            pair = l[i].split('",')
             pair[0] = pair[0][1:]
-            print(pair)
-            dicts[pair[0]] = pair[1]
-    
-    print(dicts)
+            #print(pair)
+            dicts[pair[0]] = float(pair[1])
+        else:
+            pair = l[i].split(',')
+            pair[0] = pair[0]
+            #print(pair)
+            dicts[pair[0]] = float(pair[1])
             
-        
-        
     #print(dicts)
+    #print(len(dicts))
     
+def randJob(lst):
+    print(random.choices(list(lst.keys()), weights = list(lst.values()), k = 50))
 createDict(stf)
+randJob(dicts)
