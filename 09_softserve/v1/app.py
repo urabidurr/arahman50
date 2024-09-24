6384d8f102846bbd61d1d2e963d29f2ff127ef20
Abidur Rahman <144458543+urabidurr@users.noreply.github.com>
2024-09-23 20:42:37 -0400

    Update app.py

--- 09_softserve/v1/app.py
+++ 09_softserve/v1/app.py
@@ -5,9 +5,10 @@
 from flask import Flask
 app = Flask(__name__)            #create instance of class Flask
 
-@app.route("/")                  #assign fxn to route
+@app.route("/")                  #assign fxn to route, meaning the function will be called upon accessing the http://localhost:5000/
 def hello_world():
     return "No hablo queso!"
 
-app.run()
+app.run()    #Starts the local host server to run and display the function
 
+#Instead of printing __name__, the function hello_world() only returns a string "No hablo queso!".
