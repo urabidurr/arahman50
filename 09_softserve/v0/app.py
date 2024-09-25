031881bb7766c571906a108c196a06f98c7b3a73
Abidur Rahman <144458543+urabidurr@users.noreply.github.com>
2024-09-24 22:16:05 -0400

    Update app.py

--- 09_softserve/v0/app.py
+++ 09_softserve/v0/app.py
@@ -1,14 +1,14 @@
-# Clyde 'Thluffy' Sinclair
+# Michelle Zhu, Jacob Lukose, Abidur Rahman, Evan Chan
 # SoftDev
 # September 2024
 
 from flask import Flask
-app = Flask(__name__)          # ...
+app = Flask(__name__)          # how was "__name__" defined? 
 
-@app.route("/")                # ...
+@app.route("/")                # "/" refers to root URL
 def hello_world():
-    print(__name__)            # ...
-    return "No hablo queso!"   # ...
+    print(__name__)            # prints "__main__" in terminal
+    return "No hablo queso!"   # prints on web page
 
-app.run()                      # ...
+app.run()                      # running a method of another class?
                 
