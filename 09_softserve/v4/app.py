75035a705630aecf924553347df61f7c87ca4a84
Abidur Rahman <144458543+urabidurr@users.noreply.github.com>
2024-09-24 22:22:00 -0400

    Update app.py

--- 09_softserve/v4/app.py
+++ 09_softserve/v4/app.py
@@ -1,14 +1,14 @@
-# Clyde 'Thluffy' Sinclair
+# Michelle Zhu, Jacob Lukose, Abidur Rahman, Evan Chan
 # SoftDev
 # September 2024
 
 from flask import Flask
-app = Flask(__name__)           #create instance of class Flask
+app = Flask(__name__)           # create instance of class Flask
 
-@app.route("/")                 #assign fxn to route
+@app.route("/")                 # assign fxn to route
 def hello_world():
-    print("the __name__ of this module is... ")
-    print(__name__)
+    print("the __name__ of this module is... ") # prints this right above "__main__"
+    print(__name__)      # prints "__main__"
     return "No hablo queso!"
 
 if __name__ == "__main__":      # true if this file NOT imported
