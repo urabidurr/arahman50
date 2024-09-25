f271b3266280fb1df756eb7ca46486f504b78175
Abidur Rahman <144458543+urabidurr@users.noreply.github.com>
2024-09-24 22:17:08 -0400

    Update app.py

--- 09_softserve/v2/app.py
+++ 09_softserve/v2/app.py
@@ -1,15 +1,14 @@
-# Clyde 'Thluffy' Sinclair
+# Michelle Zhu, Jacob Lukose, Abidur Rahman, Evan Chan
 # SoftDev
 # September 2024
 
 from flask import Flask
-app = Flask(__name__)             #create instance of class Flask
+app = Flask(__name__)             # create instance of class Flask
 
-@app.route("/")                   #assign fxn to route
+@app.route("/")                   # assign fxn to route
 def hello_world():
-    print("about to print __name__...")
-    print(__name__)               #where will this go?
+    print("about to print __name__...") # this prints right above "__main__"
+    print(__name__)               # this prints in the terminal
     return "No hablo queso!"
 
 app.run()
-
