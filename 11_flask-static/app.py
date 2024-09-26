ad2743931a559e18c0254ad7a99638b3f89de45c
Abidur Rahman <144458543+urabidurr@users.noreply.github.com>
2024-09-26 01:22:41 -0400

    Update app.py

--- 11_flask-static/app.py
+++ 11_flask-static/app.py
@@ -1,6 +1,10 @@
-# Clyde 'Thluffy' Sinclair
-# SoftDev
-# Sep 2024
+'''Ankita Saha, Andy Shyklo, Abidur Rahman
+  Python Pigs
+  SoftDev
+  Running Flask scripts vs. html scripts
+  2024-9-25
+  time spent: 0.67 hours
+'''
 
 # DEMO
 # basics of /static folder
@@ -14,15 +18,20 @@ def hello_world():
     print(__name__)
     return "No hablo queso!"
 
-'''
 @app.route("/static/foo.html")
 def h():
     print("the __name__ of this module is... ")
     print(__name__)
     return str(random.random())
-'''
 
+'''
+@app.route("/static/fixie.html")
+def hi():
+    print("the __name__ of this module is... ")
+    print(__name__)
+    return "<h1>Team Name: Python Pigs</h1><br><h2>Roster: Ankita Saha, Andy Shyklo, Abidur Rahman</h2>"
+'''
+    
 if __name__ == "__main__":  # true if this file NOT imported
     app.debug = True        # enable auto-reload upon code change
     app.run()
-
