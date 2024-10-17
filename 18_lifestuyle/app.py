18368ee8deba5e547871e7107aed86f5ccf7afae
Abidur Rahman <144458543+urabidurr@users.noreply.github.com>
2024-10-16 20:59:48 -0400

    Update app.py

--- 18_lifestuyle/app.py
+++ 18_lifestuyle/app.py
@@ -1,1 +1,14 @@
+from flask import Flask, render_template, request, session, redirect
+import os
 
+app = Flask(__name__)
+app.secret_key = os.urandom(32)
+
+
+@app.route('/', methods = ['GET', 'POST'])
+def dispHTML():
+    return render_template("style.html")
+
+if __name__ == "__main__": #false if this file imported as module
+    app.debug = True
+    app.run()
