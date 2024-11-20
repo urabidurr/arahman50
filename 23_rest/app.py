b314c9c8f606f5d8dcb20785f0c4ff2cc28785c1
urabidurr <arahman50@stuy.edu>
2024-11-20 10:59:37 -0500

    request

--- 23_rest/app.py
+++ 23_rest/app.py
@@ -1,1 +1,11 @@
+import urllib.request
+import json
 
+key = "qwpwSA84bvVv5XIbydb87537ZfpYRJkJlMEVddjH"
+request = "https://api.nasa.gov/planetary/apod?api_key=qwpwSA84bvVv5XIbydb87537ZfpYRJkJlMEVddjH"
+
+
+print("hello")
+
+request_url = urllib.request.urlopen('https://www.geeksforgeeks.org/')
+print(request_url.read())
