b46f4d6444661633f7cf871ddec1d5b91743f1a9
Abidur Rahman <144458543+urabidurr@users.noreply.github.com>
2024-11-20 23:57:04 -0500

    Update app.py

--- 23_rest/app.py
+++ 23_rest/app.py
@@ -1,11 +1,27 @@
-import urllib.request
-import json
+# Team Astros
+# Andy Shyklo and Abidur Rahman
+# November 20, 2024
+# 30 minutes
 
-key = "qwpwSA84bvVv5XIbydb87537ZfpYRJkJlMEVddjH"
-request = "https://api.nasa.gov/planetary/apod?api_key=qwpwSA84bvVv5XIbydb87537ZfpYRJkJlMEVddjH"
+import urllib.request, json
+from flask import render_template, Flask, session, request, redirect
 
+f = open("key_nasa.txt")
+key = f.read()
 
-print("hello")
+app = Flask(__name__)    #create Flask object
+app.secret_key = b'_5#j812jwwjkKLWio)2u'
 
-request_url = urllib.request.urlopen('https://www.geeksforgeeks.org/')
-print(request_url.read())
+@app.route("/")
+def root():
+    request_url = urllib.request.urlopen(f'https://api.nasa.gov/planetary/apod?api_key={key}')
+    readUrl = request_url.read()
+
+    x = json.loads(readUrl[:-1])
+    image1 = x["url"]
+
+    return render_template("main.html", image1=image1)
+
+if __name__ == "__main__":
+    app.debug = True 
+    app.run()
