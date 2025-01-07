f2cc5daf20c3b1ea68e3fcd00cce4cde2efba069
Abidur Rahman <144458543+urabidurr@users.noreply.github.com>
2025-01-07 00:26:52 -0500

    Update fibfac.js

--- 27_js/fibfac.js
+++ 27_js/fibfac.js
@@ -9,9 +9,23 @@
 
 //<your team's fact(n) implementation>
 
+function fact(n) {
+    if (n < 0) {
+        return undefined; 
+    } else if (n === 0 || n === 1) {
+        return 1; 
+    } else {
+        return n * fact(n - 1); 
+    }
+}
+
 //TEST CALLS
 // (writing here can facilitate EZer copy/pasting into dev console now and later...)
 
+console.log(fact(1)); 
+console.log(fact(2)); 
+console.log(fact(3)); 
+console.log(fact(4)); 
 
 //-----------------------------------------------------------------
 
@@ -20,7 +34,23 @@
 
 //<your team's fib(n) implementation>
 
+function fib(n) {
+    if (n < 0) {
+        return undefined; 
+    } else if (n === 0) {
+        return 0; 
+    } else if (n === 1) {
+        return 1; 
+    } else {
+        return fib(n - 1) + fib(n - 2); 
+    }
+}
+
 //TEST CALLS
 // (writing here can facilitate EZer copy/pasting into dev console now and later...)
 
+console.log(fib(1)); 
+console.log(fib(2)); 
+console.log(fib(3)); 
+console.log(fib(4)); 
 //=================================================================
