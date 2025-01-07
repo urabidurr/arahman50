45fb0721ca214d5b40ee7b3300f9063c59e78181
Abidur Rahman <144458543+urabidurr@users.noreply.github.com>
2025-01-07 00:18:46 -0500

    Update fibfac.scm

--- 27_js/fibfac.scm
+++ 27_js/fibfac.scm
@@ -1,1 +1,37 @@
+;Team Phantom Tollbooth :: Clyde Sinclair, Fierce Dragon
+;SoftDev pd0
+;K27 - Basic functions in JavaScript
+;2025-01-06m
 
+;Scheme antecedents for JavaScript work
+
+
+;factorial:
+(define fact (lambda (n)
+;<your team's fact(n) implementation>
+
+;TEST CALLS
+(fact 1) ;"...should be  1"
+(fact 2) ;"...should be  2"
+(fact 3) ;"...should be  6"
+(fact 4) ;"...should be  24"
+(fact 5) ;"...should be  120"
+
+
+;-----------------------------------------------------------------
+
+
+;fib:
+;<your team's fib(n) implementation>
+
+(define fib(lambda (n)
+;<your team's fib(n) implementation>
+
+;TEST CALLS
+(fib 0) ;"...should be  0"
+(fib 1) ;"...should be  1"
+(fib 2) ;"...should be  1"
+(fib 3) ;"...should be  2"
+(fib 4) ;"...should be  3"
+
+;=================================================================
